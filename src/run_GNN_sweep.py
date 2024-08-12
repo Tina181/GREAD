659fa9cc15630129b6c8b5b7058f1c9cf7c01e09
Tina181 <96771459+Tina181@users.noreply.github.com>
2024-08-12 13:05:21 +0800

    Update run_GNN_sweep.py

--- src/run_GNN_sweep.py
+++ src/run_GNN_sweep.py
@@ -4,13 +4,11 @@ import argparse
 import numpy as np
 import torch
 from torch_geometric.utils import add_remaining_self_loops, to_undirected
-
 from GNN import GNN
 from GNN_early import GNNEarly
 from data import get_dataset, set_train_val_test_split
 from heterophilic import get_fixed_splits
 from graph_rewiring import apply_beltrami
-
 from gread_params import best_params_dict, hetero_params, shared_gread_params, shared_grand_params
 from utils import dirichlet_energy
 import wandb

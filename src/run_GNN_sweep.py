bbf07eb9bccca1ae7261710ddf06e5cd4b44356e
Tina181 <1397575949@qq.com>
2024-08-12 13:00:14 +0800

    delete

--- src/run_GNN_sweep.py
+++ src/run_GNN_sweep.py
@@ -4,18 +4,14 @@ import argparse
 import numpy as np
 import torch
 from torch_geometric.utils import add_remaining_self_loops, to_undirected
-
 from GNN import GNN
 from GNN_early import GNNEarly
 from data import get_dataset, set_train_val_test_split
 from heterophilic import get_fixed_splits
 from graph_rewiring import apply_beltrami
-
 from gread_params import best_params_dict, hetero_params, shared_gread_params, shared_grand_params
 from utils import dirichlet_energy
 import wandb
-# 改变默认root path
-os.chdir('/root/autodl-tmp/GREAD/src')
 
 def get_optimizer(name, parameters, lr, weight_decay=0):
     if name == 'sgd':
